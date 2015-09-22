9134127339467c9cc85def503eea7ab255707846
Shawn Stallings <oxegeneral@yahoo.com>
2015-09-22 15:26:43 -0600

    added gender option

--- js/myApp.js
+++ js/myApp.js
@@ -2,5 +2,18 @@ var app = angular.module('myApp', []);
 
 app.controller('MyCtrl', function($scope) {
 
+	$scope.gender = '';
+
+	$scope.male = function() {
+		$scope.heShe = 'he';
+		$scope.hisHer = 'his';
+		$scope.himHer = 'him';
+	};
+	$scope.female = function() {
+		$scope.heShe = 'she';
+		$scope.hisHer = 'her';
+		$scope.himHer = 'her';
+	};
+
 });
 	
