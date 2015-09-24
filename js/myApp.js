cad0740100d2ce143517d60e8a6b97920d3ac395
Shawn Stallings <oxegeneral@yahoo.com>
2015-09-23 22:31:06 -0600

    working app with angularjs

--- js/myApp.js
+++ js/myApp.js
@@ -1,7 +1,7 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngMessages']);
 
 app.controller('MyCtrl', function($scope) {
-
+	$scope.top = 'top';
 	$scope.gender = '';
 
 	$scope.male = function() {
@@ -9,11 +9,37 @@ app.controller('MyCtrl', function($scope) {
 		$scope.hisHer = 'his';
 		$scope.himHer = 'him';
 	};
+
 	$scope.female = function() {
 		$scope.heShe = 'she';
 		$scope.hisHer = 'her';
 		$scope.himHer = 'her';
 	};
 
+	$scope.submit = function() {
+		if( $scope.myForm.$valid ) {
+			console.log("form is valid");
+			$scope.top = 'not';
+			return true;
+		}
+		else {
+			console.log("form is not valid");
+			$scope.top = 'top';
+			return false;
+		}
+	};
+
+	$scope.reset = function() {
+		$scope.top = 'top';
+		$scope.data.name = '';
+		$scope.data.jobTitle = '';
+		$scope.data.tediousTask = '';
+		$scope.data.dirtyTask = '';
+		$scope.data.celebrity = '';
+		$scope.data.uselessSkill = '';
+		$scope.data.adjective = '';
+		$scope.data.obnoxiousCelebrity = '';
+		$scope.data.hugeNumber = '';
+		$scope.myForm.$submitted = null;
+	};
 });
-	
